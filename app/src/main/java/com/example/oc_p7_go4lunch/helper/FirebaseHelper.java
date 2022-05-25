fd6c56bc83226b9910fc53898ecaffb69e82c247
Cedric Haegele <zikced@yahoo.fr>
2022-05-25 23:09:54 +0200

    update 25052022 - Maj Workmates Firebase

---
+++ app/src/main/java/com/example/oc_p7_go4lunch/helper/FirebaseHelper.java
@@ -0,0 +1,28 @@
+package com.example.oc_p7_go4lunch.helper;
+
+import com.example.oc_p7_go4lunch.model.firestore.UserModel;
+import com.firebase.ui.auth.data.model.User;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class FirebaseHelper {
+
+    private static final String COLLECTION_NAME = "users";
+
+    // collection
+    public static CollectionReference getUsersCollection() {
+        return FirebaseFirestore.getInstance().collection(COLLECTION_NAME);
+    }
+
+    // create user
+    public static Task<Void> createUser(String id, UserModel user) {
+        return FirebaseFirestore.getInstance().collection(COLLECTION_NAME).document(id).set(user);
+    }
+
+    // get all users
+    public static Task<QuerySnapshot> getUsersDocuments(){
+        return FirebaseFirestore.getInstance().collection(COLLECTION_NAME).get();
+    }
+}
